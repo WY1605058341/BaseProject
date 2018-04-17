1e61d7fb135196b90759218075b1427eb428187a
WY1605058341 <1605058341@qq.com>
2018-04-18 06:44:08 +0800

    test

---
+++ app/src/main/java/com/example/think/baseproject/model/BaseBean.java
@@ -0,0 +1,13 @@
+package com.example.think.baseproject.model;
+
+public class BaseBean {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
